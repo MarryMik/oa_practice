761f26824bd76faf95ace3c2d4e01c3470f92881
MarryMik <57491436+MarryMik@users.noreply.github.com>
2024-05-18 00:30:26 +0300

    ?

--- project/script.js
+++ project/script.js
@@ -5,7 +5,6 @@ const detail_butt1 = document.getElementsByClassName("product__href_details")[0]
                     'productCategory': 'wish',
                     'ageCategory': 'adult'
                 })
-                window.alert("Категорія товару - Wish, Вікова категорія - adult ");
         })
         const detail_butt2 = document.getElementsByClassName("product__href_details")[1];
         detail_butt2.addEventListener("click", function (e) {
@@ -14,5 +13,4 @@ const detail_butt1 = document.getElementsByClassName("product__href_details")[0]
                     'productCategory': 'greetings',
                     'ageCategory': 'child'
                 })
-                window.alert("Категорія товару - greetings, Вікова категорія - child ");
         })
