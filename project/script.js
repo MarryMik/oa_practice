1614d1f9eb01a0b7b8a09fdfc0612487d5944e5a
MarryMik <57491436+MarryMik@users.noreply.github.com>
2024-05-19 03:44:11 +0300

    ?

--- project/script.js
+++ project/script.js
@@ -1,18 +1,18 @@
-const detail_butt1 = document.getElementsByClassName("product__href_details")[0];
-        detail_butt1.addEventListener("click", function (e) {
-            e.preventDefault();
-            dataLayer.push({
-                    'productCategory': 'wish',
-                    'ageCategory': 'adult'
-                })
-        })
-        const detail_butt2 = document.getElementsByClassName("product__href_details")[1];
-        detail_butt2.addEventListener("click", function (e) {
-            e.preventDefault();
-            dataLayer.push({
-                    'productCategory': 'greetings',
-                    'ageCategory': 'child'
-                })
-        })
+// const detail_butt1 = document.getElementsByClassName("product__href_details")[0];
+//         detail_butt1.addEventListener("click", function (e) {
+//             e.preventDefault();
+//             dataLayer.push({
+//                     'productCategory': 'wish',
+//                     'ageCategory': 'adult'
+//                 })
+//         })
+//         const detail_butt2 = document.getElementsByClassName("product__href_details")[1];
+//         detail_butt2.addEventListener("click", function (e) {
+//             e.preventDefault();
+//             dataLayer.push({
+//                     'productCategory': 'greetings',
+//                     'ageCategory': 'child'
+//                 })
+//         })
 
         
